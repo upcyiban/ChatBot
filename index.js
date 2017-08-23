a6a4edf494b2b5176e15a2a5838debee9372cd4b
wulei123 <1534563365@qq.com>
2017-08-23 17:30:17 +0800

    update for heroku

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ let builder = require('botbuilder')
 let app = express()
 
 
-app.get('/',(req, res, next)=>{
-    res.send('This is Chat Bot!!!')
-})
 
 let connector = new builder.ChatConnector({
     appId: '3685e916-b8a6-4f3d-8a9e-e4e9231c25a7',
     appPassword: 'Dgxxxf9DJVk31qnN6sZM9RV'
 })
 
+app.get('/',(req, res, next)=>{
+    res.send('This is Chat Bot!!!')
+})
+
 app.post('/api/messages',connector.listen())
 
 let bot = new builder.UniversalBot(connector, (session)=>{
-    session.send('Hello World')
+    // session.send('Hello World')
 })
 
-app.listen(process.env.PORT||5000, function() {
-    console.log('Node app is running on port', app.get('port'));
+bot.dialog('greeting',(session)=>{
+    session.endDialog('Hello')
+}).triggerAction({
+    matches: 'My.Test'
+})
+
+const luis_url = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/1d9245db-703c-4326-a5e6-2811a63b0189?subscription-key=4a094653359e49af8f7c11ccd8e81902&timezoneOffset=0&verbose=true&q='
+bot.recognizer(new builder.LuisRecognizer(luis_url));
+
+let server = app.listen(process.env.PORT||5000, ()=> {
+    console.log('Node app is running on port',server.address().port);
 });
